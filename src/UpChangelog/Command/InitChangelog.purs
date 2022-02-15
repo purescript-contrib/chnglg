64d3abf22bbcf2b70427fe740d138ad02a168960
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-14 20:00:49 -0600

    Generate initial CHANGELOG.md file if not exist

--- src/UpChangelog/Command/InitChangelog.purs
+++ src/UpChangelog/Command/InitChangelog.purs
@@ -2,11 +2,14 @@ module UpChangelog.Command.InitChangelog where
 
 import Prelude
 
+import Data.String as String
 import Effect.Aff (Aff)
 import Effect.Class.Console (log)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff as FSA
+import Node.Path (sep)
 import Node.Path as Path
+import UpChangelog.Command.GenChangelog (commaSeparate)
 import UpChangelog.Constants as Constants
 import UpChangelog.Git (git)
 
@@ -15,9 +18,24 @@ initChangelog force = do
   unlessM (FSA.exists Constants.changelogDir) do
     FSA.mkdir Constants.changelogDir
   let readme = Path.concat [ Constants.changelogDir, Constants.readmeFile ]
-  readmeExists <- FSA.exists Constants.readmeFile
-  if (readmeExists && not force) then do
-    log $ Constants.readmeFile <> " already exists. Not overwriting. You can use the --force flag to override this"
-  else do
-    FSA.writeTextFile UTF8 readme Constants.readmeContent
-    void $ git "add" [ "-q", readme ]
+  attemptToAddFile readme Constants.readmeContent
+  attemptToAddFile Constants.changelogFile Constants.changelogContent
+  where
+  attemptToAddFile file content = do
+    fileExists <- FSA.exists file
+    if (fileExists && not force) then do
+      log $ alreadyExistsWarning file
+    else do
+      FSA.writeTextFile UTF8 file content
+      void $ git "add" [ "-q", file ]
+
+  alreadyExistsWarning file = String.joinWith " "
+    [ file
+    , "already exists, not overwriting."
+    , "You can use the --force flag to overwrite all files generated"
+    , "by this command (i.e. " <> commaSeparate generatedFiles <> ")"
+    ]
+  generatedFiles =
+    [ Constants.changelogDir <> sep <> Constants.readmeFile
+    , Constants.changelogFile
+    ]
