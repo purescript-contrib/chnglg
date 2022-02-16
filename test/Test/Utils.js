34f2dd601adc231b05bfc5053d62f33ce5ce2ab0
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-16 12:40:13 -0600

    Fix FFI by running the effect

--- test/Test/Utils.js
+++ test/Test/Utils.js
@@ -3,8 +3,7 @@ const fs = require("fs");
 exports.mkdtemp = (prefix) => (cbLeft) => (cbRight) => () =>
   fs.mkdtemp(prefix, "utf-8", (err, dir) => {
     if (err) {
-      cbLeft(err);
+      cbLeft(err)();
     }
-    console.log("Got: " + dir);
-    cbRight(dir);
+    cbRight(dir)();
   });
