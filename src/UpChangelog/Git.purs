03a1700ae4462b5869e2ce40a1469c21b52c3c3e
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-09-17 07:52:31 -0500

    Drop redundant arg

--- src/UpChangelog/Git.purs
+++ src/UpChangelog/Git.purs
@@ -3,7 +3,6 @@ module UpChangelog.Git where
 import Prelude
 
 import Data.Array as Array
-import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (liftAff)
 import Node.Library.Execa (ExecaResult, execa)
 import UpChangelog.App (App)
@@ -12,4 +11,4 @@ import UpChangelog.App (App)
 -- | be sure to wrap the file names in quotes. Otherwise, those with spaces
 -- | may cause errors on Linux-like OSes.
 git :: forall r. String -> Array String -> App r ExecaResult
-git cmd args = liftAff $ _.getResult =<< execa "git" (Array.cons cmd args) (_ { cwd = Just "." })
+git cmd args = liftAff $ _.getResult =<< execa "git" (Array.cons cmd args) identity
